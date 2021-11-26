d25545ae835058a606cd9a448c37b6748d500cfc
Chris Troutner <chris.troutner@gmail.com>
2021-11-26 06:02:35 -0800

    Successfully connected to wallet service

---
+++ dev-docs/README.md
@@ -0,0 +1,9 @@
+# Developer Documentation
+
+This directory contains documentation for developers.
+
+## Theory of Operation
+
+This app provides a bridge between the Web 2.0 REST API world and the Web3.0 IPFS world. Web apps (like wallet.fullstack.cash) can interact with the REST API provided by the this web server app. This app then pulls the back-end infrastructure through a the IPFS network.
+
+When started, this app will spin up an IPFS node and connect to the PSF network using [ipfs-coord](https://www.npmjs.com/package/ipfs-coord). It will find and connect with a BCH wallet service provider via IPFS. Clients connecting to the REST API can then access BCH wallet infrastructure via the REST API.
--- src/adapters/ipfs/ipfs-coord.js
+++ src/adapters/ipfs/ipfs-coord.js
@@ -8,10 +8,15 @@
 const IpfsCoord = require('ipfs-coord')
 const BCHJS = require('@psf/bch-js')
 const publicIp = require('public-ip')
+const semver = require('semver')
 
 // Local libraries
 const config = require('../../../config')
 // const JSONRPC = require('../../controllers/json-rpc/')
+
+// The minimum version of ipfs-bch-wallet-service that this wallet can work with.
+const MIN_BCH_WALLET_VERSION = '1.11.0'
+const WALLET_PROTOCOL = 'bch-wallet'
 
 let _this
 
@@ -24,6 +29,12 @@ class IpfsCoordAdapter {
         'Instance of IPFS must be passed when instantiating ipfs-coord.'
       )
     }
+    // this.eventEmitter = localConfig.eventEmitter
+    // if (!this.eventEmitter) {
+    //   throw new Error(
+    //     'An instance of an EventEmitter must be passed when instantiating the ipfs-coord adapter.'
+    //   )
+    // }
 
     // Encapsulate dependencies
     this.IpfsCoord = IpfsCoord
@@ -31,9 +42,19 @@ class IpfsCoordAdapter {
     this.bchjs = new BCHJS()
     this.config = config
     this.publicIp = publicIp
+    this.semver = semver
 
     // Properties of this class instance.
     this.isReady = false
+
+    // Periodically poll services for available wallet service providers.
+    this.pollServiceInterval = setInterval(this.pollForServices, 10000)
+
+    // State object. TODO: Make this more robust.
+    this.state = {
+      serviceProviders: [],
+      selectedServiceProvider: ''
+    }
 
     _this = this
   }
@@ -90,6 +111,84 @@ class IpfsCoordAdapter {
       throw err
     }
   }
+
+  // Poll the ipfs-coord coordination channel for available service providers.
+  // This method is called periodically by a timer-interval.
+  pollForServices () {
+    try {
+      // An array of IPFS IDs of other nodes in the coordination pubsub channel.
+      const peers = _this.ipfsCoord.thisNode.peerList
+      // console.log(`peers: ${JSON.stringify(peers, null, 2)}`)
+
+      // Array of objects. Each object is the IPFS ID of the peer and contains
+      // data about that peer.
+      const peerData = _this.ipfsCoord.thisNode.peerData
+      // console.log(`peerData: ${JSON.stringify(peerData, null, 2)}`)
+
+      for (let i = 0; i < peers.length; i++) {
+        const thisPeer = peers[i]
+        const thisData = peerData.filter((x) => x.from === thisPeer)
+        const thisPeerData = thisData[0]
+
+        // Create a 'fingerprint' that defines the wallet service.
+        const protocol = thisPeerData.data.jsonLd.protocol
+        const version = thisPeerData.data.jsonLd.version
+        // console.log(
+        //   `debug: peer ${thisPeer} uses protocol: ${protocol} v${version}`,
+        // )
+
+        let versionMatches = false
+        if (version) {
+          versionMatches = _this.semver.gt(version, MIN_BCH_WALLET_VERSION)
+        }
+
+        // Ignore any peers that don't match the fingerprint for a BCH wallet
+        // service.
+        if (protocol && protocol.includes(WALLET_PROTOCOL) && versionMatches) {
+          // console.log('Matching peer: ', thisPeerData)
+
+          // Temporary business logic.
+          // Use the first available wallet service detected.
+          if (_this.state.serviceProviders.length === 0) {
+            _this.state.selectedServiceProvider = thisPeer
+
+            // Persist the config setting, so it can be used by other commands.
+            // _this.conf.set('selectedService', thisPeer)
+            console.log(`---->BCH wallet service selected: ${thisPeer}`)
+          }
+
+          // Add the peer to the list of serviceProviders.
+          _this.state.serviceProviders.push(thisPeer)
+        }
+      }
+    } catch (err) {
+      console.error('Error in pollForServices(): ', err)
+      // Do not throw error. This is a top-level function.
+    }
+  }
+
+  // This method handles input coming in from other IPFS peers.
+  // It passes the data on to the REST API library by emitting an event.
+  // peerInputHandler (data) {
+  //   try {
+  //     // console.log('peerInputHandler triggered with this data: ', data)
+  //
+  //     _this.eventEmitter.emit('rpcData', data)
+  //   } catch (err) {
+  //     console.error('Error in ipfs-coord.js/peerInputHandler(): ', err)
+  //     // Do not throw error. This is a top-level function.
+  //   }
+  // }
 }
+
+// Create a random number to use in the name of this IPFS n ode.
+// const randNum = Math.floor(Math.random() * 10000)
+
+// const announceJsonLd = {
+//   '@context': 'https://schema.org/',
+//   '@type': 'Person',
+//   name: `wallet-consumer-${randNum}`,
+//   description: 'A consumer of BCH wallet services'
+// }
 
 module.exports = IpfsCoordAdapter
