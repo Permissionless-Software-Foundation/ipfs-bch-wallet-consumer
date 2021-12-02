3a30dd6b03f88ecd10dbc0866165c52fa081f1b4
Chris Troutner <chris.troutner@gmail.com>
2021-12-02 14:08:33 -0800

    fix(mongo): Changing db name

--- config/env/development.js
+++ config/env/development.js
@@ -7,6 +7,6 @@
 module.exports = {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/ipfs-service-dev',
+  database: 'mongodb://localhost:27017/ipfs-bch-consumer-dev',
   env: 'dev'
 }
